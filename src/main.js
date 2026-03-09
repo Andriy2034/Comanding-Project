744578bdd1dbbdb8639400119df4e7203de06177
Anriy <andrijmilevskij@gmail.com>
2026-03-09 07:24:15 +0200

    add modal

--- src/main.js
+++ src/main.js
@@ -1,11 +1,30 @@
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelectorAll('[data-modal-open]'),
+//     closeModalBtn: document.querySelector('[data-modal-close]'),
+//     modal: document.querySelector('[data-modal]'),
+//   };
+
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
+
+//   function toggleModal() {
+//     refs.modal.classList.toggle('is-hidden');
+//     document.body.classList.toggle('no-scroll');
+//   }
+// })();
+
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
+    openModalBtn: document.querySelectorAll('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.forEach(btn => {
+    btn.addEventListener('click', toggleModal);
+  });
+
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
